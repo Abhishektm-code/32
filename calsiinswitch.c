d8f723affd04dac901306cde4d9f7726128d2dca
abhishek <tmabhishek14@gmail.com>
2024-10-30 21:38:24 +0530

    Add files via upload

---
+++ calsiinswitch.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char op;
+    int n1,n2,n3;
+    printf("enter an operater (+,-,*,/)");
+    scanf("%c",&op);
+    printf("enter two numbers:");
+    scanf("%d%d",)
+}
